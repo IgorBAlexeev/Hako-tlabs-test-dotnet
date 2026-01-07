9f7f5e42df431dfa066504e8d38bc197576173e4
Igor Alexeev <ibalexeev@gmail.com>
2026-01-07 21:17:07 +0400

    1. Reference for authentication Microsoft.AspNetCore.Authentication.JwtBearer has been added 2. Startup.cs: changes for - in-memory cache - cors - swagger - authentication

--- Server/src/TestApp.Server/Startup.cs
+++ Server/src/TestApp.Server/Startup.cs
@@ -1,8 +1,13 @@
-using TestApp.ToDoList.Store;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.Net.Http.Headers;
+using Microsoft.OpenApi.Models;
+using System.Text;
 using TestApp.ToDoList.Module;
-using TestApp.ToDoList.Tracker;
 using TestApp.ToDoList.Repository;
-
+using TestApp.ToDoList.Store;
+using TestApp.ToDoList.Tracker;
 
 namespace TestApp.Server
 {
@@ -20,28 +25,83 @@ namespace TestApp.Server
       services.AddDbContext<ToDoListDbContext>();
 
       // Add controllers
-      services.AddControllers();
+      services.AddControllers(options => {
+        options.CacheProfiles.Add("NoCache",
+        new CacheProfile() { NoStore = true });
+        options.CacheProfiles.Add("Any-120",
+        new CacheProfile()
+        {
+          Location = ResponseCacheLocation.Any,
+          Duration = 120
+        });
+      });
 
       // Configure app services
       services.AddScoped<IToDoListTracker, ToDoListTracker>();
       services.AddScoped<IToDoItemsRepository, ToDoItemsRepository>();
       services.AddScoped<ToDoListEntityModel>();
-
+      var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
       services.AddCors(options =>
       {
-        options.AddDefaultPolicy(policy =>
-        {
-          policy.AllowAnyOrigin()
-            .AllowAnyHeader();
-        });
+        options.AddPolicy(name: MyAllowSpecificOrigins,
+            builder =>
+            {
+              builder.WithOrigins("http://localhost:5173") // Allow requests from your frontend's exact origin
+                 .AllowAnyHeader()
+                 .AllowAnyMethod();
+            });
       });
-
       services.AddEndpointsApiExplorer();
       services.AddSwaggerGen();
+
+      services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+      options.TokenValidationParameters = new TokenValidationParameters
+      {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = configuration["Jwt:Issuer"],
+        ValidAudience = configuration["Jwt:Audience"],
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["Jwt:Key"]))
+      };
+    });
+      services.AddAuthorization();
+      services.AddEndpointsApiExplorer();
+      services.AddSwaggerGen(options =>
+      {
+        options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+        {
+          In = ParameterLocation.Header,
+          Description = "Please enter token",
+          Name = "Authorization",
+          Type = SecuritySchemeType.Http,
+          BearerFormat = "JWT",
+          Scheme = "bearer"
+        });
+        options.AddSecurityRequirement(new OpenApiSecurityRequirement
+        {
+          {
+            new OpenApiSecurityScheme
+            {
+              Reference = new OpenApiReference
+              {
+                Type=ReferenceType.SecurityScheme,
+                Id="Bearer"
+              }
+            },
+            Array.Empty<string>()
+          }
+        });
+      });
+      services.AddMemoryCache();
+      // Todo: uncomment
+      // services.AddExceptionHandler<GlobalExceptionHandler>();
     }
     public void Configure(IApplicationBuilder app, IWebHostEnvironment env, IServiceProvider svcProv)
     {
-
       // Enable Swagger in all environments
       app.UseSwagger();
       app.UseSwaggerUI();
@@ -50,21 +110,19 @@ namespace TestApp.Server
       appLifetime.ApplicationStarted.Register(onApplicationStarted);
 
       app.UseRouting();
+      app.UseCors("_myAllowSpecificOrigins");
+      app.UseAuthentication();
       app.UseAuthorization();
-
-      app.UseCors();
-
+      app.UseExceptionHandler(_ => { });
       app.UseEndpoints(endpoints =>
-        {
-          endpoints.MapControllers();
-        }
+      {
+        endpoints.MapControllers();
+      }
       );
     }
-
     void onApplicationStarted()
     {
       // Do nothing
     }
-
   }
 }
